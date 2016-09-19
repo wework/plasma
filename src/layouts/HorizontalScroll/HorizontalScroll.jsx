ce22677b5861d99697ab1c22a48d4ffd728acf0e
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2016-09-19 13:49:21 -0400

    onScroll prop for HorizontalScroll

--- src/layouts/HorizontalScroll/HorizontalScroll.jsx
+++ src/layouts/HorizontalScroll/HorizontalScroll.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import $ from 'jquery';
+import _ from 'lodash';
 import style from './style.scss';
 
 class HorizontalScroll extends React.Component {
+
+  componentDidMount() {
+    $(this.wrapper).on('scroll', this.throttledHandleScroll);
+  }
+
+  componentDidUnmount() {
+    $(this.wrapper).off('scroll');
+  }
+
+  handleScroll(e) {
+    this.props.onScroll(e);
+  }
+
   render() {
     return (
-      <div className={style.wrapper}>
-        { this.props.children }
+      <div className={style.wrapper} ref={(c) => { this.wrapper = c; }}>
+        <div className={style.content}>
+          { this.props.children }
+        </div>
       </div>
     );
   }
@@ -17,6 +34,7 @@ HorizontalScroll.defaultProps = {
 
 HorizontalScroll.propTypes = {
   children: React.PropTypes.node,
+  onScroll: React.PropTypes.func,
 };
 
 HorizontalScroll.displayName = 'HorizontalScroll';
