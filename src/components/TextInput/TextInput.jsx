33e67de16a51fcf9d0dac252417f305631afaf07
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2016-11-22 12:16:06 -0500

    added prefix and suffix props to TextInput

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import cx from 'classnames';
 import style from './style.scss';
 
 class TextInput extends React.Component {
   render() {
+    const wrapperStyle = cx(style.wrapper, {
+      [style.hasPrefix]: this.props.prefix,
+      [style.hasSuffix]: this.props.suffix,
+    });
+
+    const fixStyle = cx(style.fix, {
+      [style.suffix]: this.props.suffix,
+      [style.prefix]: this.props.prefix,
+    });
+
     return (
-      <input
-        type="text"
-        placeholder={this.props.placeholder}
-        className={style.input}
-        value={this.props.value}
-        onChange={this.props.onChange}
-      />
+      <div className={wrapperStyle}>
+        <input
+          type="text"
+          placeholder={this.props.placeholder}
+          className={style.input}
+          value={this.props.value}
+          onChange={this.props.onChange}
+        />
+        <div className={fixStyle}>
+          {this.props.suffix || this.props.prefix}
+        </div>
+      </div>
     );
   }
 }
@@ -19,6 +35,8 @@ TextInput.propTypes = {
   placeholder: React.PropTypes.string,
   value: React.PropTypes.string,
   onChange: React.PropTypes.func,
+  suffix: React.PropTypes.node,
+  prefix: React.PropTypes.node,
 };
 
 TextInput.defaultProps = {
