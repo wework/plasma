ecbea32e15087e090d547e99b1a61d95e0dcd650
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2017-03-24 12:24:47 -0400

    add version scripts

---
+++ preversion.js
@@ -0,0 +1,8 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+const p = require('./package.json');
+const oldVersion = p.version;
+
+fs.createReadStream(`./design/Sketch43/Plasma@${oldVersion}.sketch`)
+  .pipe(fs.createWriteStream(`./design/Sketch43/Plasma-temp.sketch`));
