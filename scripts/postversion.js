2a6a1244fb01709c99bbef4bb5683559ac97e6f5
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2017-03-24 15:30:42 -0400

    futzing with version scripts

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -1,3 +1,3 @@
 const execSync = require('child_process').execSync;
 
-const gitPush = execSync('git push && git push --tags');
+const gitPush = execSync('git push origin master && git push origin --tags');
