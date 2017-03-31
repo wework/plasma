5c2d6d2362fe44a5cd46414592b71662a585a272
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2017-03-30 23:46:22 -0400

    another checkbox example

--- src/components/Checkbox/examples.js
+++ src/components/Checkbox/examples.js
@@ -2,6 +2,7 @@
   return {
     'Unchecked': <Checkbox text='I agree to all of it' />,
     'Checked': <Checkbox checked text='I agree to all of it' />,
-    'Indeterminate': <Checkbox indeterminate text='Chicago' />,
+    'Indeterminate unchecked': <Checkbox indeterminate text='Chicago' />,
+    'Indeterminate checked': <Checkbox indeterminate checked text='Chicago' />,
   };
 })
