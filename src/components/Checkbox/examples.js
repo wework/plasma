2d1e537920cfee719ad182ec29519f982504e5d4
Nicholas Stamas <nstamas@nyc-020194-mbp.local>
2017-03-30 23:41:56 -0400

    add checkbox examples

---
+++ src/components/Checkbox/examples.js
@@ -0,0 +1,7 @@
+(function() {
+  return {
+    'Unchecked': <Checkbox text='I agree to all of it' />,
+    'Checked': <Checkbox checked text='I agree to all of it' />,
+    'Indeterminate': <Checkbox indeterminate text='Chicago' />,
+  };
+})
