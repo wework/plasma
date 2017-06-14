4e5ee9d3946160ecf876380db2fc61f9672ae6f9
Nicholas Stamas <nicholas.stamas@gmail.com>
2017-06-14 11:13:55 -0400

    redesigning side nav

---
+++ src/components/DisplayText/DisplayText.jsx
@@ -0,0 +1,39 @@
+import cx from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Base from '../Base.jsx';
+import style from './style.scss';
+
+class DisplayText extends React.Component {
+  render() {
+    const headerStyle = cx(style.header, {
+      [style.header1]: this.props.h1,
+      [style.header2]: this.props.h2,
+      [style.header3]: this.props.h3,
+      [style.header4]: this.props.h4,
+      [style.header5]: this.props.h5,
+      [style.header6]: this.props.h6,
+    });
+
+    return (
+      <div className={[headerStyle]}>
+        <div className={style.content}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+DisplayText.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.string.oneOf(['default', 'extraLarge', 'large', 'medium', 'small']),
+};
+
+DisplayText.defaultProps = {
+  children: 'Display text',
+};
+
+DisplayText.displayName = 'DisplayText';
+
+export default Base(DisplayText);
