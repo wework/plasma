7e7bc59e2f831e9516ef49e02ae79304f1dfdf13
Everest Guerra <everest.guerra@wework.com>
2017-06-20 14:40:24 -0400

    MT-2396 fixed lint errors

--- src/components/Toggles/Toggles.jsx
+++ src/components/Toggles/Toggles.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import _ from'lodash';
+import _ from 'lodash';
 
 import Base from '../Base.jsx';
 import style from './style.scss';
@@ -12,7 +12,7 @@ const type = {
   VERTICAL: 'vertical',
 };
 
-class Toggles extends React.Component{
+class Toggles extends React.Component {
   onClick(toggle, index) {
     this.props.onChange(toggle.label, index);
   }
@@ -25,10 +25,11 @@ class Toggles extends React.Component{
 
   render() {
     return (
-      <ul className={style.toggles} role="togglelist">
+      <ul className={style.toggles} role="tablist">
         { _.map(this.props.items, (toggle, index) => {
           const toggleClasses = cx(style.toggle, {
-            [style.active]: (this.props.selectedIndex === index || this.props.selectedLabel === toggle.label),
+            [style.active]: (this.props.selectedIndex === index ||
+                             this.props.selectedLabel === toggle.label),
             [style.disabled]: this.props.items[index].disabled,
             [style.horizontal]: this.props.type === type.HORIZONTAL,
             [style.vertical]: this.props.type === type.VERTICAL,
@@ -36,7 +37,7 @@ class Toggles extends React.Component{
 
           return (
             <li
-              role="toggle"
+              role="tab"
               onClick={() => this.onClick(toggle, index)}
               onKeyDown={(event) => this.onKeyDown(event, toggle, index)}
               className={toggleClasses}
@@ -73,5 +74,3 @@ Toggles.propTypes = {
 Toggles.displayName = 'Toggles';
 
 export default Base(Toggles);
-
-
