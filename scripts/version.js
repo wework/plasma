005bb64c6ddec16fd40fad3dffe9dbc47172090f
Nicholas Stamas <nicholas.stamas@wework.com>
2017-06-28 14:42:52 -0400

    update version scripts

--- scripts/version.js
+++ scripts/version.js
@@ -9,9 +9,4 @@ if (fs.existsSync('./dist/styles.css')) {
   fs.unlinkSync('./dist/styles.css');
 }
 
-const webpack = execSync('webpack --config webpack.config.prod');
-const p = require('../package.json');
-fs.renameSync(`./design/Sketch43/Plasma-temp.sketch`, `./design/Sketch43/Plasma@${p.version}.sketch`);
-const sketchFiles = fs.readdirSync('./design/Sketch43');
-sketchFiles.filter(name => new RegExp(`^Plasma@(?!${p.version}).*$`).test(name)).forEach(name => fs.unlinkSync(`./design/Sketch43/${name}`));
-const gitAddDesign = execSync('git add -A design');
+execSync('webpack --config webpack.config.prod');
