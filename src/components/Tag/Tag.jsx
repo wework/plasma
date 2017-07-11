e7ad7301f1d22ddfc0794373ef137924f745669e
Aaron Couture <aaron.couture@wework.com>
2017-07-11 18:33:07 -0400

    Add tag component to plasma

---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import Base from '../Base.jsx';
+import style from './style.scss';
+
+const Tag = ({ text, isLarge, isLeft }) => {
+  const tagClasses = cn(style.tag, style.small)
+  return (
+    <div className={tagClasses}>
+      <span>{text}</span>
+    </div>
+  );
+};
+
+Tag.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Base(Tag);
