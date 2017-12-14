4ce23ce06042f94325de86ae3354275c71bfea97
Aaron Couture <aaron.couture@wework.com>
2017-12-14 17:45:13 -0500

    add support for onBlur so it is complient with redux-form

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,15 +1,11 @@
-import { uniqueId } from 'lodash';
-import React from 'react';
-import PropTypes from 'prop-types';
-import cx from 'classnames';
-import style from './style.scss';
-import {
-  getDataAttrs,
-  getDataProps }
-from '../../dataUtils';
+import { uniqueId } from "lodash";
+import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
+import style from "./style.scss";
+import { getDataAttrs, getDataProps } from "../../dataUtils";
 
 class Checkbox extends React.Component {
-
   render() {
     const {
       isBold,
@@ -21,26 +17,22 @@ class Checkbox extends React.Component {
       disabled,
       value,
       data,
+      onBlur
     } = this.props;
-    const id = uniqueId('id');
+    const id = uniqueId("id");
     const inputClassName = indeterminate ? style.indeterminate : style.original;
     const wrapperClassName = cx(style.wrapper, {
-      [style.wrapperDisabled]: disabled,
+      [style.wrapperDisabled]: disabled
     });
     const textClassName = cx(style.text, {
       [style.textBold]: isBold,
-      [style.textDisabled]: disabled,
+      [style.textDisabled]: disabled
     });
 
     return (
-      <div
-        {...getDataAttrs(data)}
-      >
+      <div {...getDataAttrs(data)}>
         <div>
-          <label
-            htmlFor={id}
-            className={wrapperClassName}
-          >
+          <label htmlFor={id} className={wrapperClassName}>
             <input
               disabled={disabled}
               checked={checked || value}
@@ -49,18 +41,15 @@ class Checkbox extends React.Component {
               id={id}
               name={name}
               onChange={onChange}
+              onBlur={onBlur}
             />
             <div className={style.checkbox} />
-            <div className={textClassName}>
-              {text}
-            </div>
+            <div className={textClassName}>{text}</div>
           </label>
         </div>
-        { this.props.description &&
-          <span className={style.description}>
-            { this.props.description }
-          </span>
-        }
+        {this.props.description && (
+          <span className={style.description}>{this.props.description}</span>
+        )}
       </div>
     );
   }
@@ -70,8 +59,8 @@ Checkbox.defaultProps = {
   checked: false,
   value: false,
   indeterminate: false,
-  text: 'Option',
-  name: 'checkbox',
+  text: "Option",
+  name: "checkbox"
 };
 
 Checkbox.propTypes = {
@@ -81,15 +70,16 @@ Checkbox.propTypes = {
   text: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.node,
-    PropTypes.element,
+    PropTypes.element
   ]).isRequired,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func,
   disabled: PropTypes.bool,
   description: PropTypes.string,
-  ...getDataProps(),
+  onBlur: PropTypes.func,
+  ...getDataProps()
 };
 
-Checkbox.displayName = 'Plasma@Checkbox';
+Checkbox.displayName = "Plasma@Checkbox";
 
 export default Checkbox;
