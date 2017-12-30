c368cef600907cddf028dbe72483c3266e6253ff
Aaron Couture <aaron.couture@wework.com>
2017-12-30 14:19:37 -0500

    missed loader file when fixing lint errors

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import style from "./style.scss";
-import { getDataAttrs, getDataProps } from "../../dataUtils";
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.scss';
+import { getDataAttrs, getDataProps } from '../../dataUtils';
 
 class Loader extends React.Component {
   render() {
     return (
-      <div
-        {...getDataAttrs(this.props.data)}
-        className={style.container}
-        style={this.props.style}
-      >
+      <div {...getDataAttrs(this.props.data)} className={style.container} style={this.props.style}>
         <div className={style.spinner}>
           <div className={style.bounce1} style={this.props.dotStyle} />
           <div className={style.bounce2} style={this.props.dotStyle} />
@@ -22,15 +18,15 @@ class Loader extends React.Component {
 }
 
 Loader.defaultProps = {
-  dotStyle: { backgroundColor: "#59698B" }
+  dotStyle: { backgroundColor: '#59698B' },
 };
 
 Loader.propTypes = {
   style: PropTypes.object,
   dotStyle: PropTypes.object,
-  ...getDataProps()
+  ...getDataProps(),
 };
 
-Loader.displayName = "Plasma@Loader";
+Loader.displayName = 'Plasma@Loader';
 
 export default Loader;
