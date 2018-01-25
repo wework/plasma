cb6667ea9dc4794a082b4f187fe6752874476764
Aaron Couture <aaron.couture@wework.com>
2018-01-25 14:33:23 -0500

    add test for prop

--- src/tests/__tests__/OverflowMenu.react-test.js
+++ src/tests/__tests__/OverflowMenu.react-test.js
@@ -7,9 +7,13 @@ import { shallow, mount } from 'enzyme';
 
 describe('OverflowMenu Component', () => {
   test('render', () => {
-    const component = renderer.create(
-      <OverflowMenu />
-    );
+    const component = renderer.create(<OverflowMenu />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('render with label', () => {
+    const component = renderer.create(<OverflowMenu label="Hello World!" />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
