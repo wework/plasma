7c785d9f101e9fa25089cb17fa4d9dbcaa6903f1
pseuyi <pseuyi@gmail.com>
2018-03-06 23:21:49 -0500

    add fancy radio buttons to stories

--- stories/RadioButton.js
+++ stories/RadioButton.js
@@ -4,6 +4,56 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import RadioButton from '../src/components/RadioButton/RadioButton';
 
+class FancyRadioButtons extends React.Component {
+  state = {
+    selected: "Building Announcements"
+  }
+  handleCheck = (e) => {
+    this.setState({ selected: e.target.value })
+  }
+  render() {
+    return (
+      <div>
+        <RadioButton
+          text="Need To Know"
+          fieldValue="Need To Know"
+          fancy
+          onChange={this.handleCheck}
+          value={this.state.selected}
+        />
+        <RadioButton
+          text="House Rules"
+          fieldValue="House Rules"
+          fancy
+          onChange={this.handleCheck}
+          value={this.state.selected}
+        />
+        <RadioButton
+          text="Limited Staffing"
+          fieldValue="Limited Staffing"
+          fancy
+          onChange={this.handleCheck}
+          value={this.state.selected}
+        />
+        <RadioButton
+          text="Fire Drill"
+          fieldValue="Fire Drill"
+          fancy
+          onChange={this.handleCheck}
+          value={this.state.selected}
+        />
+        <RadioButton
+          text="Emergency"
+          fieldValue="Emergency"
+          fancy
+          onChange={this.handleCheck}
+          value={this.state.selected}
+        />
+      </div>
+    );
+  }
+}
+
 storiesOf('RadioButton', module)
   .add('with Text', () => (
     <RadioButton text="Text" />
@@ -16,10 +66,13 @@ storiesOf('RadioButton', module)
   ))
   .add('with Text and Long Description and isLarge', () => (
     <RadioButton text="Text" isLarge description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed diam sem. Quisque mollis pulvinar metus eget dignissim. Pellentesque ac neque magna. Vivamus vel sapien lectus. Ut at aliquet urna. Proin volutpat odio sed diam maximus tristique. Ut pellentesque venenatis nisi, in faucibus risus feugiat at. Pellentesque consectetur auctor mollis. Morbi neque nulla, suscipit at leo suscipit, vestibulum tristique metus. Curabitur volutpat consectetur risus, nec commodo eros placerat interdum. Sed eget mi mi. Quisque eget pharetra dui, eu accumsan dui. Nullam non lacinia odio, ac pellentesque metus. Nulla purus metus, elementum eu tristique at, interdum sed urna. Vivamus vulputate enim ac orci lacinia elementum." />
-  ))  
+  ))
   .add('multiple', () => (
     <div>
       <RadioButton text="One" description="This is the first RadioButton" />
       <RadioButton text="Two" description="This is the second RadioButton" />
     </div>
+  ))
+  .add('fancy', () => (
+    <FancyRadioButtons />
   ));
