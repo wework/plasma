1626a737469d65d63a1a72df8a00abd1bde8fe31
Chelsea Kochan <chelsea.kochan@wework.com>
2018-03-29 10:20:55 -0400

    added collapsibble card component to storybook

---
+++ stories/CollapsibleCard.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import CollapsibleCard from '../src/components/CollapsibleCard/CollapsibleCard';
+
+storiesOf('CollapsibleCard', module)
+  .add('default', () => (
+    <div style={{ display: 'flex' }}>
+      <CollapsibleCard
+        headerText="Header Text"
+      >
+        Hello
+      </CollapsibleCard>
+    </div>
+  ))
+  .add('expandable text', () => (
+    <div style={{ display: 'flex' }}>
+      <CollapsibleCard
+        headerText="Header Text"
+        expandableText="more"
+      >
+        Hello
+      </CollapsibleCard>
+    </div>
+  ))
+  .add('red style color', () => (
+    <div style={{ display: 'flex' }}>
+      <CollapsibleCard
+        headerText="Header Text"
+        styleColor="red"
+      >
+        Hello
+      </CollapsibleCard>
+    </div>
+  ))
+  .add('disabled', () => (
+    <div style={{ display: 'flex' }}>
+      <CollapsibleCard
+        headerText="Header Text"
+        disabled
+      >
+        Hello
+      </CollapsibleCard>
+    </div>
+  ));
