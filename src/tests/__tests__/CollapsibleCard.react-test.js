c25d2ad126f42c8193f009372544bb3c586e64c2
Chelsea Kochan <chelsea.kochan@wework.com>
2018-03-29 11:10:10 -0400

    added tests for collapsibble card component

---
+++ src/tests/__tests__/CollapsibleCard.react-test.js
@@ -0,0 +1,90 @@
+/* eslint-disable */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CollapsibleCard from '../../components/CollapsibleCard/CollapsibleCard';
+
+describe('CollapsibleCard Component unit', () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(
+      <CollapsibleCard
+        headerText="Header Text"
+        expandableText="more"
+      >
+        Test Card
+      </CollapsibleCard>
+    );
+  })
+
+  test('displays header text', () => {
+    expect(component.contains('Header Text')).toBe(true);
+  });
+
+  test('displays expandable text', () => {
+    expect(component.contains('more')).toBe(true);
+  });
+
+  test('displays arrow down', () => {
+    expect(component.find('.textLinkArrowDown').exists()).toBe(true);
+  });
+
+  test('no red border color', () => {
+    expect(component.is('.styleColorRed')).toBe(false);
+  });
+
+  test('red border color', () => {
+    component = shallow(
+      <CollapsibleCard
+        headerText="Header Text"
+        styleColor="red"
+      >
+        Test Red Card
+      </CollapsibleCard>
+    );
+    expect(component.is('.styleColorRed')).toBe(true);
+  });
+
+  describe('when clicked ', () => {
+    test('displays children', () => {
+      component.find('.textLink').simulate('click');
+      expect(component.contains('Test Card')).toBe(true);
+    });
+
+    test('displays arrow up', () => {
+      component.find('.textLink').simulate('click');
+      expect(component.find('.textLinkArrowUp').exists()).toBe(true);
+    });
+  });
+
+  describe('when disabled ', () => {
+    beforeEach(() => {
+      component = shallow(
+        <CollapsibleCard
+          headerText="Header Text"
+          expandableText="more"
+          disabled
+        >
+          Test Card
+        </CollapsibleCard>
+      );
+    });
+
+    test('displays header text', () => {
+      expect(component.contains('Header Text')).toBe(true);
+    });
+
+    test('does not display arrow down', () => {
+      expect(component.find('.textLinkArrowDown').exists()).toBe(false);
+    });
+
+    test('does not display arrow up', () => {
+      expect(component.find('.textLinkArrowUp').exists()).toBe(false);
+    });
+
+    test('does not display children', () => {
+      expect(component.contains('Test Card')).toBe(false);
+    });
+  });
+});
