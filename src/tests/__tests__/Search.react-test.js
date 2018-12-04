e6b5cbb881b38e7af199103e3a85c69fda03541e
Amir Grozki <amirgrozki@gmail.com>
2018-12-04 18:21:18 +0200

    Fixed tests for Search.

--- src/tests/__tests__/Search.react-test.js
+++ src/tests/__tests__/Search.react-test.js
@@ -8,23 +8,39 @@ import { shallow, mount } from 'enzyme';
 describe('Search Component', () => {
   test('render', () => {
     const component = renderer.create(
-      <Search />
+      <Search
+        items={[]}
+        icon={<img src="http://yolo.com/icon.png" />}
+      />
     );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
+  });
+
+  test('render value', () => {
+    const component = renderer.create(
+      <Search
+        items={[]}
+        clearable
+        clearIcon={<img src="http://yolo.com/icon.png" />}
+        value="test"
+      />
+    );
+
+    expect(component).toMatchSnapshot();
   });
 
   test('onClear', () => {
     const onClear = jest.fn();
     const component = mount(
       <Search
+        items={[]}
         clearable
-        clearIconUrl='http://yolo.com/icon.png'
+        clearIcon={<img src="http://yolo.com/icon.png" />}
         onClear={onClear}
       />
     );
-    component.setState({ text: 'text' });
-    component.find('.clearableIcon').simulate('click');
+    component.setState({ value: 'text' });
+    component.find('button').simulate('click');
     expect(onClear).toHaveBeenCalledTimes(1);
   });
 
@@ -32,11 +48,17 @@ describe('Search Component', () => {
     const onChange = jest.fn();
     const component = mount(
       <Search
+        items={[]}
         onChange={onChange}
       />
     );
-    component.find('input').simulate('change', { target: { value: 'YOLO' }});
+
+    const input = component.find('input');
+
+    input.getDOMNode().value = 'YOLO';
+    input.simulate('change');
+
     expect(onChange).toHaveBeenCalledTimes(1);
-    expect(component.state('text')).toBe('YOLO');
+    expect(component.state('value')).toBe('YOLO');
   });
 });
