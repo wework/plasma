a4128754a0f3e8b1de764211a067541e1e95d5db
Amir Grozki <amirgrozki@gmail.com>
2019-01-24 17:08:00 +0200

    Added GlobalAttributes to Hint.

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -2,20 +2,23 @@
 import React, { type Node } from 'react';
 import cx from 'classnames';
 import { getDataAttrs } from '../../dataUtils';
-import type { Data } from '../../types';
+import type {
+  Data,
+  GlobalAttributes,
+} from '../../types';
 
 import styles from './style.scss';
 
 type Props = {|
   children?: Node,
-  className?: string,
   data?: Data,
   disabled?: boolean,
   error?: boolean,
   text?: string,
+  ...GlobalAttributes,
 |};
 
-const Hint = ({ children, className, data, disabled, error, text }: Props) => {
+const Hint = ({ children, className, data, disabled, error, text, ...rest }: Props) => {
   const classes = cx(styles.hint, className, {
     [styles.hintDisabled]: disabled,
     [styles.hintError]: error,
@@ -25,6 +28,7 @@ const Hint = ({ children, className, data, disabled, error, text }: Props) => {
     <div
       className={classes}
       {...getDataAttrs(data)}
+      {...rest}
     >
       {children || text}
     </div>
