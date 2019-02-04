d3809bdc297a16f681af863911ee780f8c3e8bc8
Amir Grozki <amirgrozki@gmail.com>
2019-02-04 15:46:50 +0200

    Updated snapshots and tests.

--- src/tests/__tests__/Checkbox.react-test.js
+++ src/tests/__tests__/Checkbox.react-test.js
@@ -7,14 +7,25 @@ import renderer from 'react-test-renderer';
 describe('Checkbox Component', () => {
   test('render', () => {
     const component = renderer.create(<Checkbox />);
+
     let tree = component.toJSON();
+
     expect(tree).toMatchSnapshot();
+
     tree.props.checked = true;
+
     expect(tree).toMatchSnapshot();
   });
 
-  test('render', () => {
-    const component = renderer.create(<Checkbox fancy />);
+  test('render disabled', () => {
+    const component = renderer.create(<Checkbox disabled />);
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('render error', () => {
+    const component = renderer.create(<Checkbox error />);
+
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
