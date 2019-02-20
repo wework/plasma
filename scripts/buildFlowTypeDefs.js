f03e50ca5e7af1a5a3c3491f51eab59ff480b6b3
kangax <kangax@gmail.com>
2019-02-20 14:12:14 -0500

    Fix scoping

--- scripts/buildFlowTypeDefs.js
+++ scripts/buildFlowTypeDefs.js
@@ -13,10 +13,10 @@ const declarations = execSync(
 console.log(`
 // flow-typed version: <<STUB>>/@wework-dev/plasma_v${currentVersion}/flow_v${flowVersion}
 
-${String(declarations).replace(/\bNode\b/g, 'React$Node')}
-
 declare module '@wework-dev/plasma' {
   declare type Data = { key: string, value: string };
+
+  ${String(declarations).replace(/\bNode\b/g, 'React$Node')}
 
   declare export class CountedTextInput extends React$Component<{ ...$PropertyType<TextInput, 'props'>, counterStyle: string }> { }
   declare export class CountedTextArea extends React$Component<{ ...$PropertyType<TextArea, 'props'>, counterStyle: string }> { }
