8312463a0370ebf7bca67d401a4c220f488d7724
Amir Grozki <amirgrozki@gmail.com>
2019-03-14 15:13:36 +0200

    Minor fixes.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,19 @@
 // @flow
 import React, { type Node } from 'react';
-import style from './style.scss';
 import Header from '../Header/Header';
+
 import { getDataAttrs } from '../../dataUtils';
+
 import type { Data } from '../../types';
+
+import styles from './style.scss';
 
 type Props = {|
   title: string,
   actions: Array<Node>,
   children: Node,
   visible?: boolean,
-  onDismiss?: (evt: MouseEvent) => void,
+  onDismiss?: (evt: SyntheticMouseEvent<>) => void,
   innerCardStyle: { [string]: string | number },
   minWidth?: string | number,
   maxWidth?: string | number,
@@ -36,21 +39,21 @@ class Modal extends React.Component<Props> {
     }
 
     return (
-      <div {...getDataAttrs(data)} className={style.wrapper}>
-        <div className={style.innerWrapper}>
-          <div style={{ ...innerCardStyle, minWidth, maxWidth }} className={style.card}>
-            <div className={style.content}>
+      <div {...getDataAttrs(data)} className={styles.wrapper}>
+        <div className={styles.innerWrapper}>
+          <div style={{ ...innerCardStyle, minWidth, maxWidth }} className={styles.card}>
+            <div className={styles.content}>
               {title && (
-                <div className={style.top}>
+                <div className={styles.top}>
                   <Header h3>{title}</Header>
                 </div>
               )}
               {children}
-              {actions && <div className={style.pageActions}>{actions}</div>}
+              {actions && <div className={styles.pageActions}>{actions}</div>}
             </div>
           </div>
         </div>
-        <div className={style.overlay} onClick={onDismiss} />
+        <div className={styles.overlay} onClick={onDismiss} />
       </div>
     );
   }
