d5d2d39f32b131ced4beaf0fc7542a30d1f9789e
aviv.oren <aviv.oren@wework.com>
2019-03-18 13:51:22 +0200

    timepicker component update react select version add moment

--- src/components/TimePicker/TimePicker.jsx
+++ src/components/TimePicker/TimePicker.jsx
@@ -1,43 +1,229 @@
 // @flow
-import React, { Component } from 'react';
-import style from './style.scss';
-import type { Data } from '../../types';
+import React from 'react';
+import moment from 'moment';
+import cn from 'classnames';
+import ReactSelect from 'react-select';
+import styles from './style.scss';
 
-type Props = {|
-  onBlur?: (event: { target: { value: Object } }) => void,
-  onChange?: (event: { target: { value: Object } }) => void,
-  time: string,
-  data?: Data,
-  disabled?: boolean,
+/**
+ * A TimePicker component.
+ *
+ * Optional 'value' must be in 24H 'HH:mm' format.
+ *
+ * Optional 'minTime' in 24H 'HH:mm' format, default time 00:00.
+ *
+ * Optional 'maxTime' in 24H 'HH:mm' format, default time 24:00.
+ *
+ * Optional 'timeIntervalMinutes', Time step for time slots, default is 30 minute.
+ *
+ * Optional 'timeFormatType' label type in 24H or 12H, default 12H.
+ *
+ * Optional 'defaultOption':
+ *    nextInterval = will round up: 11:26 -> 11:30  based on 'timeIntervalMinutes'.
+ *    minimum (default selection) =  will select 'minTime'.
+ *
+ * Optional 'menuIsOpen': Whether dropdown is open. Default: false.
+ *
+ * Optional 'disabled'.
+ *
+ * Optional 'error'.
+ *
+ * Optional 'required'.
+ *
+ * Optional 'name'.
+ *
+ * Optional 'id'.
+ *
+ * Optional 'tabIndex'.
+ *
+ * onChange receives string value in 24H 'HH:mm' format.
+ *
+ * Optional 'onBlur'.
+ *
+ * Optional 'onFocus'.
+ */
+
+export const FormatTypes = Object.freeze({
+  timeFormat12: 'hh:mm A',
+  timeFormat24: 'HH:mm',
+});
+
+export type TimeFormatType = $Values<typeof FormatTypes>;
+
+export const DefaultOptions = Object.freeze({
+  minimum: 'minimum',
+  nextInterval: 'nextInterval',
+});
+
+export type DefaultOption = $Values<typeof DefaultOptions>;
+
+type Option = {|
+  label: string,
+  value: string,
 |};
 
-class TimePicker extends Component<Props> {
-  static displayName = 'Plasma@TimePicker';
+type Props = {|
+  maxTime: string,
+  minTime: string,
+  defaultOption?: DefaultOption,
+  timeFormat: TimeFormatType,
+  timeIntervalMinutes: number,
+  value?: string,
+  borderless?: boolean,
+  className?: string,
+  disabled?: boolean,
+  error?: boolean,
+  hidden?: boolean,
+  id?: string,
+  menuIsOpen?: boolean,
+  onBlur?: (?string) => void,
+  onChange?: string => void,
+  onFocus?: (SyntheticFocusEvent<>) => void,
+  placeholder?: string,
+  name?: string,
+  tabIndex?: string | number,
+  title?: string,
+|};
 
-  handleBlur = (event: { target: { value: Object } }): void => {
-    const newTime = event.target.value;
+type State = {|
+  options: Array<Option>,
+|};
 
-    this.props.onBlur && this.props.onBlur(newTime);
+const moment24h = (value?: string): moment => moment(value, FormatTypes.timeFormat24);
+
+const enumerateOptions = (
+  start: string,
+  end: string,
+  intervalMinutes: number,
+  timeFormat: string
+): Array<Option> => {
+  const minTime = moment24h(start);
+  const maxTime = moment24h(end);
+
+  const timeValue = minTime.clone();
+  const options = [];
+
+  while (timeValue.isSameOrBefore(maxTime)) {
+    options.push({
+      value: timeValue.format(FormatTypes.timeFormat24),
+      label: timeValue.format(timeFormat),
+    });
+
+    timeValue.add(intervalMinutes, 'minutes');
+  }
+
+  return options;
+};
+
+const ClockIcon = () => <span className={styles.selectClockIcon} />;
+
+class TimePicker extends React.Component<Props, State> {
+  static defaultProps = {
+    minTime: '00:00',
+    maxTime: '24:00',
+    timeFormat: FormatTypes.timeFormat12,
+    timeIntervalMinutes: 30,
+    placeholder: 'Select time',
   };
 
-  handleChange = (event: { target: { value: Object } }): void => {
-    const newTime = event.target.value;
+  static getDerivedStateFromProps = ({
+    maxTime,
+    minTime,
+    timeFormat,
+    timeIntervalMinutes,
+  }: Props) => ({
+    options: enumerateOptions(minTime, maxTime, timeIntervalMinutes, timeFormat),
+  });
 
-    this.props.onChange && this.props.onChange(newTime);
+  getValue(value: ?string): ?string {
+    if (!value) {
+      return this.defaultTimeSelected();
+    }
+
+    const { maxTime, minTime } = this.props;
+
+    const start = moment24h(minTime);
+    const end = moment24h(maxTime);
+    const valueMoment = moment24h(value);
+
+    if (valueMoment.isBefore(start)) {
+      return minTime;
+    }
+
+    if (valueMoment.isAfter(end)) {
+      return maxTime;
+    }
+
+    return value;
+  }
+
+  handleChange = (option: Option) => {
+    const { onChange } = this.props;
+    if (onChange) onChange(option.value);
   };
+
+  handleOnBlur = (): void => {
+    const { onBlur, value } = this.props;
+    if (onBlur) onBlur(value);
+  };
+
+  defaultTimeSelected(): ?string {
+    const { timeIntervalMinutes, defaultOption } = this.props;
+    switch (defaultOption) {
+      case DefaultOptions.nextInterval: {
+        const roundedUp = Math.ceil(moment().minute() / timeIntervalMinutes) * timeIntervalMinutes;
+
+        return moment()
+          .minute(roundedUp)
+          .second(0)
+          .format(FormatTypes.timeFormat24);
+      }
+      case DefaultOptions.minimum:
+        return moment24h(this.props.minTime).format(FormatTypes.timeFormat24);
+      default:
+        return null;
+    }
+  }
 
   render() {
-    const { time, disabled } = this.props;
+    const {
+      borderless,
+      className,
+      disabled,
+      error,
+      menuIsOpen,
+      value,
+      placeholder,
+      ...rest
+    } = this.props;
+
+    const timeSelectClassName = cn(styles.selectInput, className, {
+      [styles.borderless]: borderless,
+      [styles.selectInputError]: error,
+    });
+
+    const currentValue = this.getValue(value);
+
+    const currentOption = this.state.options.find(item => item.value === currentValue);
 
     return (
-      <input
-        disabled={disabled}
-        className={style.component}
-        type="time"
-        name="timePicker"
-        onBlur={this.handleBlur}
+      <ReactSelect
+        {...rest}
+        menuIsOpen={menuIsOpen}
+        components={{
+          IndicatorSeparator: null,
+          DropdownIndicator: ClockIcon,
+        }}
+        className={timeSelectClassName}
+        classNamePrefix="time-picker"
+        clearable={false}
+        options={this.state.options}
+        searchable={false}
+        isDisabled={disabled}
+        value={currentOption}
         onChange={this.handleChange}
-        value={time}
+        placeholder={placeholder}
+        onBlur={this.handleOnBlur}
       />
     );
   }
--- stories/TimePicker.js
+++ stories/TimePicker.js
@@ -1,12 +1,72 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import TimePicker from '../src/components/TimePicker/TimePicker';
+import TimePicker, { DefaultOptions, FormatTypes } from '../src/components/TimePicker/TimePicker';
+import StoryState from '../StoryState';
 
 storiesOf('TimePicker', module)
-  .add('default', () => (
-    <TimePicker onBlur={action('blurred')} onChange={action('changed')} time="20:00" />
+  .add('open', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker value={state.value} onChange={value => setState({ value })} menuIsOpen />
+      )}
+    />
   ))
   .add('disabled', () => (
-    <TimePicker disabled onBlur={action('blurred')} onChange={action('changed')} time="20:00" />
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker value={state.value} onChange={value => setState({ value })} disabled />
+      )}
+    />
+  ))
+  .add('error', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker onChange={value => setState({ value })} value={state.value} error />
+      )}
+    />
+  ))
+  .add('borderless', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker
+          borderless
+          value={state.value || '14:00'}
+          onChange={value => setState({ value })}
+        />
+      )}
+    />
+  ))
+  .add('24H format', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker
+          timeFormat={FormatTypes.timeFormat24}
+          value={state.value || '14:00'}
+          onChange={value => setState({ value })}
+        />
+      )}
+    />
+  ))
+  .add('12H format', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker
+          timeFormat={FormatTypes.timeFormat12}
+          value={state.value || '14:00'}
+          onChange={value => setState({ value })}
+        />
+      )}
+    />
+  ))
+  .add('default option minimum', () => (
+    <StoryState
+      render={(state, setState) => (
+        <TimePicker
+          defaultOption={DefaultOptions.minimum}
+          value={state.value}
+          onChange={value => setState({ value })}
+        />
+      )}
+    />
   ));
