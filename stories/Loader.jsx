7af70009ef5aaa0455bd98df4215fecfeef65011
Kobi Meirson <kobi.meirson@wework.com>
2019-06-20 14:21:49 +0300

    Make sure dotStyle still works. Have eslint stop yelling because of storybook import

--- stories/Loader.jsx
+++ stories/Loader.jsx
@@ -6,5 +6,6 @@ import styles from './style.scss';
 
 storiesOf('Loader', module)
   .add('default', () => <Loader />)
-  .add('dotColor', () => <Loader dotColor="#ff0000" />)
+  .add('dotStyle', () => <Loader dotStyle={{ backgroundColor: '#c1c1c1' }} />)
+  .add('dotColor', () => <Loader dotColor="#f00" />)
   .add('dotClassName', () => <Loader dotClassName={styles.greenDotLoader} />);
