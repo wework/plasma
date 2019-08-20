ca602f1489bdd57150c9ca0d7384a374f719384f
Naftali Lubin <naftalilubin@gmail.com>
2019-08-20 11:40:54 -0400

    reduce repeats in the generated flow typed file

--- scripts/extractComponentFlowTypes.js
+++ scripts/extractComponentFlowTypes.js
@@ -1,8 +1,10 @@
 export default function extractDefaultFlowTypes(file, api) {
   const j = api.jscodeshift;
   const root = j(file.source);
+  let hasDeclaration = false;
 
   root.find(j.ClassDeclaration).forEach(() => {
+    hasDeclaration = true;
     const componentName = file.path.match(/(\w+)\.jsx/)[1];
     const filePath = file.path.replace('src', 'lib').replace('.jsx', '');
 
@@ -11,5 +13,18 @@ declare module "@wework-dev/plasma/${filePath}" {
   declare module.exports: $PropertyType<$Exports<"@wework-dev/plasma">, "${componentName}">;
 }`);
   });
+
+  if (!hasDeclaration) {
+    root.find(j.VariableDeclaration).forEach(() => {
+      const componentName = file.path.match(/(\w+)\.jsx/)[1];
+      const filePath = file.path.replace('src', 'lib').replace('.jsx', '');
+
+      console.log(`
+declare module "@wework-dev/plasma/${filePath}" {
+  declare module.exports: $PropertyType<$Exports<"@wework-dev/plasma">, "${componentName}">;
+}`);
+    });
+  }
+
   return file.source;
 }
